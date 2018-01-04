53f57c86e4dfa4f9ff2b7454f28149e69667b44b
Webmaster444 <jlmobile710@gmail.com>
2018-01-04 16:51:37 +0100

    index page edited

--- js/script.js
+++ js/script.js
@@ -6,5 +6,18 @@ $('.menu-toggle').click(function() {
 })
 
 $(document).ready(function(){    
-    $("#home_advance").trigger("click");            
+    var s = Math.random()*20;
+        s = s % 4;
+        sv = Math.floor(s);
+        console.log(sv);
+        if(sv==0){
+          $('#freq1').show();
+          $('#freq3').hide();
+          $("#home_advance").trigger("click");            
+        }else{
+          $('#freq3').show();
+          $('#freq1').hide();
+          $("#home_advance1").trigger("click");            
+        }
+    
 })
