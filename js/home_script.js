64708ec57565d3360300c217c34f501e15007b8a
Webmaster444 <jlmobile710@gmail.com>
2018-01-11 10:21:10 +0100

    homepage done

--- js/home_script.js
+++ js/home_script.js
@@ -6,4 +6,11 @@ $(function(){
 	$('body').on('mousewheel', function (event) {
 		window.location.href="behaviour.html";
 	});
+
+	var mousewheelevt=(/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+ 
+	if (document.attachEvent) //if IE (and Opera depending on user setting)
+	    document.attachEvent("on"+mousewheelevt, function(e){window.location.href="behaviour.html";})
+	else if (document.addEventListener) //WC3 browsers
+	    document.addEventListener(mousewheelevt, function(e){window.location.href="behaviour.html";}, false)
 })
