cafde4f73c933ae794a7bd2356478c8209764e95
Webmaster444 <jlmobile710@gmail.com>
2018-01-11 09:48:30 +0100

    scroll event added

--- js/home_script.js
+++ js/home_script.js
@@ -2,4 +2,8 @@ $(function(){
 	$(window).scroll(function() {
 	  window.location.href="behaviour.html";
 	});
+
+	$('body').on('mousewheel', function (event) {
+		window.location.href="behaviour.html";
+	});
 })
